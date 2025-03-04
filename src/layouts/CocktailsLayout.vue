f3f4c35cc759bb7d15f662d473bdbb3adb20be36
Grigorii <vnukovg@gmail.com>
2025-03-05 01:23:18 +0400

    add cocktail layout

---
+++ src/layouts/CocktailsLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+</script>
+
+<template>
+  <NavBar />
+
+  <main>
+    <slot></slot>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 2rem;
+}
+</style>
--- src/views/CocktailView.vue
+++ src/views/CocktailView.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
-import NavBar from '@/components/NavBar.vue'
 import CoctailList from '@/components/CoctailList.vue'
+import CocktailsLayout from '@/layouts/CocktailsLayout.vue'
 
 const route = useRoute()
 const type = computed<string>(() => route.params.type as string)
 </script>
 
 <template>
-  <NavBar />
-
-  <main>
+  <CocktailsLayout>
     <CoctailList :type="type" />
-  </main>
+  </CocktailsLayout>
 </template>
-
-<style scoped>
-main {
-  padding: 2rem;
-}
-</style>
