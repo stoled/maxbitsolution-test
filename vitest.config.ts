19724e92ab17ea584f4e677af1fe4eb5813960e3
Grigorii <vnukovg@gmail.com>
2025-03-04 22:01:17 +0400

    add global scss styling

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,5 +10,12 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/base.scss";',
+        },
+      },
+    },
   }),
 )
